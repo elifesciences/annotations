639e0e93206167ad7c7a32c4a3000744f0ffeff9
nlisgo <nathan@aeopus.com>
2017-11-16 21:40:17 +0000

    Undo changes to smoke_tests.sh

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-set -ex
+. /opt/smoke.sh/smoke.sh
 
-[ $(curl --write-out %{http_code} --silent --output /dev/null localhost/ping) == 200 ]
+bin/console --version --env=$ENVIRONMENT_NAME
+
+smoke_url_ok $(hostname)/ping
+    smoke_assert_body "pong"
+
+smoke_report
