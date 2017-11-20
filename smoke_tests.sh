bab7f1b790ebc9483e71b0d4c33382ceb18002c5
Giorgio Sironi <g.sironi@elifesciences.org>
2017-11-20 13:34:24 +0000

    Failing if bin/console fails

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 . /opt/smoke.sh/smoke.sh
+set -e
 
 bin/console --version --env=$ENVIRONMENT_NAME
 
+# smoke tests need to run while allowing errors
+set +e
 smoke_url_ok $(hostname)/ping
     smoke_assert_body "pong"
 
