e30dbb7e4d7b121ce0e9a74049a1b72fb9f3c997
Giorgio Sironi <g.sironi@elifesciences.org>
2017-12-06 13:29:37 +0000

    -i seems not necessary to run PHPUnit

--- container_tests.sh
+++ container_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-docker run -it annotations-cli /usr/bin/env php /srv/annotations/vendor/bin/phpunit
+docker run -t annotations-cli /usr/bin/env php /srv/annotations/vendor/bin/phpunit
