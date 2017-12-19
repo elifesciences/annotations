880ec53abf75d6401a92de440c5bef9555453512
Giorgio Sironi <g.sironi@elifesciences.org>
2017-12-19 16:08:30 +0000

    Allowing for host and port override in smoke tests

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -4,9 +4,13 @@ set -e
 
 bin/console --version
 
+host="${1:-$(hostname)}"
+port="${2:-80}"
+
 # smoke tests need to run while allowing errors
 set +e
-smoke_url_ok $(hostname)/ping
+curl -v "$host:$port/ping"
+smoke_url_ok "$host:$port/ping"
     smoke_assert_body "pong"
 
 smoke_report
