d5bf7ec2f188b8f40cb072422e988c6c7381bdc7
nlisgo <nathan@aeopus.com>
2018-01-24 13:58:19 +0000

    Remove redundant functions

--- src/Annotations/Serializer/CommonMark/functions.php
+++ src/Annotations/Serializer/CommonMark/functions.php
@@ -11,69 +11,15 @@ function clean_paragraph($text)
     return strip_tags($text, $allowed_tags);
 }
 
-function escape_mathml($text)
-{
-    return preg_replace_callback('~(?P<before><math[^>]*>)(?P<mathml>.*)(?P<after></math>)~s', function ($match) {
-        return Xml::escape($match['before'].$match['mathml'].$match['after']);
-    }, $text);
-}
-
-function encode_mathml($text)
-{
-    return preg_replace_callback('~(?P<before><math[^>]*>)(?P<mathml>.*)(?P<after></math>)~s', function ($match) {
-        return encode_string($match['before'].$match['mathml'].$match['after'], true, 'MATH');
-    }, $text);
-}
-
-function decode_mathml($text)
-{
-    return decode_string($text, 'MATH');
-}
-
-function escape_latex($text)
-{
-    return preg_replace_callback('~(?P<before>\$\$)(?P<latex>.+)(?P<after>\$\$)~s', function ($match) {
-        return Xml::escape($match['before'].$match['latex'].$match['after']);
-    }, $text);
-}
-
-function encode_latex($text)
-{
-    return preg_replace_callback('~(?P<before>\$\$)(?P<latex>.+)(?P<after>\$\$)~s', function ($match) {
-        return encode_string($match['before'].$match['latex'].$match['after'], true, 'LATEX');
-    }, $text);
-}
-
-function decode_latex($text)
-{
-    return decode_string($text, 'LATEX');
-}
-
 function escape_math($text)
 {
-    return escape_latex(escape_mathml($text));
-}
-
-function encode_math($text)
-{
-    return encode_latex(encode_mathml($text));
-}
-
-function decode_math($text)
-{
-    return decode_latex(decode_mathml($text));
-}
-
-function encode_string($text, $escape = false, $prefix = '')
-{
-    return '|'.$prefix.'ENCODESTART|'.base64_encode($escape ? Xml::escape($text) : $text).'|'.$prefix.'ENCODEEND|';
-}
-
-function decode_string($text, $prefix = '')
-{
-    $preg_prefix = preg_quote($prefix, '~');
-
-    return preg_replace_callback('~(?P<before>\|'.$preg_prefix.'ENCODESTART\|)(?P<string>.+)(?P<after>\|'.$preg_prefix.'ENCODEEND\|)~s', function ($match) {
-        return base64_decode($match['string']);
+    // Escape MathML.
+    $escaped = preg_replace_callback('~(?P<before><math[^>]*>)(?P<mathml>.*)(?P<after></math>)~s', function ($match) {
+        return Xml::escape($match['before'].$match['mathml'].$match['after']);
     }, $text);
+    // Escape LaTeX.
+    $escaped = preg_replace_callback('~(?P<before>\$\$)(?P<latex>.+)(?P<after>\$\$)~s', function ($match) {
+        return Xml::escape($match['before'].$match['latex'].$match['after']);
+    }, $escaped);
+    return $escaped;
 }
