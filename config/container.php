9d46cbf7d0965ec76749b59dcbec5916a5869184
Giorgio Sironi <g.sironi@elifesciences.org>
2018-01-26 11:22:00 +0000

    Container only allows HTTP

--- config/container.php
+++ config/container.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'api_url' => 'https://api_dummy:8080',
+    'api_url' => 'http://api_dummy:8080',
     'hypothesis' => [
         // TODO: should be hypothesis-dummy
         'api_url' => 'https://hypothes.is/api/',
