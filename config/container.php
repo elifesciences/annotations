b80ea319b4e5b357031fabea765dbc761ca80dcd
Giorgio Sironi <g.sironi@elifesciences.org>
2018-01-30 10:45:01 +0000

    Correct hypothesis_dummy hostname

--- config/container.php
+++ config/container.php
@@ -3,7 +3,7 @@
 return [
     'api_url' => 'http://api_dummy:8080',
     'hypothesis' => [
-        'api_url' => 'http://hypothes_dummy:8080/',
+        'api_url' => 'http://hypothesis_dummy:8080/',
     ],
     'aws' => [
         'queue_name' => 'annotations--dev',
