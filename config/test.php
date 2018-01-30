5543ae8fe64fca956cbb2d8ca5d6081757544762
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-01-30 11:45:42 +0000

    Improve error logging

--- config/test.php
+++ config/test.php
@@ -2,6 +2,7 @@
 
 $config = require __DIR__.'/ci.php';
 
+$config['api_problem']['factory']['include_exception_details'] = false;
 $config['aws']['stub'] = true;
 $config['api_url'] = 'https://api.elifesciences.org/';
 $config['mock'] = true;
