2f2cfe30c66fdbc51e6cbd07630e658ba19a043c
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-02 17:53:07 +0000

    Unify entry point to web/app.php

---
+++ web/app.php
@@ -0,0 +1,15 @@
+<?php
+
+use eLife\Annotations\AppKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new AppKernel(getenv('ENVIRONMENT_NAME'));
+
+$request = Request::createFromGlobals();
+
+$response = $app->handle($request);
+$response->send();
+
+$app->terminate($request, $response);
