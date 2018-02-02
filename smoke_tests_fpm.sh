54a4f83dcec009bde0761ad70eca1e5cbb8d6665
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-02 15:50:30 +0000

    Separate smoke tests for cli and fpm

---
+++ smoke_tests_fpm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# TODO: we should need only one web/app.php file?
+# use $ENVIRONMENT_NAME with a default?
+ping=$(SCRIPT_FILENAME=/srv/annotations/web/app_dev.php/ping REQUEST_URI=/ping REQUEST_METHOD=GET cgi-fcgi -bind -connect 127.0.0.1:9000 | tail -n 1)
+[ "$ping" == "pong" ]
+
