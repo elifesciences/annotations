11cedbedab1106e8e805d071860c03ab67a3468f
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-02 16:00:53 +0000

    Improve verbosity of fpm smoke test

--- smoke_tests_fpm.sh
+++ smoke_tests_fpm.sh
@@ -4,5 +4,6 @@ set -e
 # TODO: we should need only one web/app.php file?
 # use $ENVIRONMENT_NAME with a default?
 ping=$(SCRIPT_FILENAME=/srv/annotations/web/app_dev.php/ping REQUEST_URI=/ping REQUEST_METHOD=GET cgi-fcgi -bind -connect 127.0.0.1:9000 | tail -n 1)
+echo "GET /ping: $ping"
 [ "$ping" == "pong" ]
 
