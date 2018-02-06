54877b38927fb5ac1ef12c7a1fab7d35ab25fbd8
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-06 17:42:12 +0000

    Remind to improve error message

--- smoke_tests_fpm.sh
+++ smoke_tests_fpm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
+set -o pipefail
 
+# TODO: specify expected output in call to curl_fpm, rename assert_fpm
 ping=$(curl_fpm /ping | tail -n 1)
 echo "GET /ping: $ping"
 [ "$ping" == "pong" ]
