4e9c20a9337404d9a767d5eb35a5961791980a54
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-15 14:25:53 +0000

    Switch from curl_fpm to assert_fpm

--- smoke_tests_fpm.sh
+++ smoke_tests_fpm.sh
@@ -2,7 +2,4 @@
 set -e
 set -o pipefail
 
-# TODO: specify expected output in call to curl_fpm, rename assert_fpm
-ping=$(curl_fpm /ping | tail -n 1)
-echo "GET /ping: $ping"
-[ "$ping" == "pong" ]
+assert_fpm "/ping" "pong"
