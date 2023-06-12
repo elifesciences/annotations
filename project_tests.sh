b00f8da337093d9174e1d721b9ea6fc3831641d0
LSH <lsh-0@users.noreply.github.com>
2023-06-12 10:25:52 +0930

    removes proofreader-php (#764)

--- project_tests.sh
+++ project_tests.sh
@@ -3,9 +3,5 @@ set -e
 
 rm -f build/*.xml
 
-echo "proofreader"
-proofreader bin/ src/ web/
-proofreader --no-phpcpd tests/
-
 echo "PHPUnit tests"
 vendor/bin/phpunit --log-junit build/phpunit.xml
